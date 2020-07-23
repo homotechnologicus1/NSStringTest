11d56a40927ceca8a100386e4f7e1e2cce8ab13a
joe <homo.technologicus1@gmail.com>
2020-07-23 15:54:16 +0900

    Initial Commit

---
+++ NSStringTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSStringTest
+//
+//  Created by joe_mac on 07/23/2020.
+//  Copyright © 2020 Joe K. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
