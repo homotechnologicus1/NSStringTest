d0ad28f5ec27332b478537c8e9e24865f7a81a25
joe <homo.technologicus1@gmail.com>
2020-07-23 19:35:57 +0900

    Objective-C training

--- NSStringTest/main.m
+++ NSStringTest/main.m
@@ -10,8 +10,40 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+//        NSString *str = [[NSString alloc]init];
+//        str = @"This is NSString";
+        
+//        NSString *str = [[NSString alloc]initWithString:@"This is NSString"];
+        // convenience method: initWith로 시작
+        
+        NSString *str = @"This is NSString";
+        NSLog(@"str : %@", str);
+        
+        // immutable class
+        NSString *result;
+        result = [str substringFromIndex:6];
+        result = [str substringToIndex:3];
+        
+        // method chain
+        result = [[str substringToIndex:11]substringFromIndex:8];
+        result = [[str substringFromIndex:8]substringToIndex:3];
+        
+        result = [str substringWithRange:NSMakeRange(8, 3)];
+        
+        result = [[str substringWithRange:NSMakeRange(8, 3)]lowercaseString];
+        NSLog(@"result : %@", result);
+
+        
+        // NSMutableString
+        // convenience method: stringWith로 시작
+        NSMutableString *mstr = [NSMutableString stringWithString:str];
+        
+        // appendString has no return value(void) (∵ NSMutableString)
+        // making changes to itself
+        [mstr appendString:@" and NSMutableString"];
+        [mstr insertString:@"Mutable " atIndex:8];
+        NSLog(@"mstr : %@", mstr);
+        
     }
     return 0;
 }
